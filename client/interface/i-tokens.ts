4932daa000c77eabda87f0ff940845c6a88bbd82
anarchy sano <pasindu.metaroon@gmail.com>
2024-02-08 13:19:34 +0530

    feat: integrate blockchain data with garphql and urql

---
+++ client/interface/i-tokens.ts
@@ -0,0 +1,6 @@
+export interface ITokens {
+    contentURI: string;
+    id: string;
+    tokenID: string
+    tokenIPFSPath: string
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,59 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import {cacheExchange, Client, fetchExchange} from 'urql'
+import {ITokens} from "../interface/i-tokens.ts";
+
+
+const GRAPH_API_URL = "https://api.thegraph.com/subgraphs/name/sano0007/learn-subgraph"
+
+const query = `
+query {
+  tokens(first: 5) {
+    id
+    tokenID
+    contentURI
+    tokenIPFSPath
+  }
+  users(first: 5) {
+    id
+    tokens {
+      id
+    }
+    created {
+      id
+    }
+  }
+}
+`
+
+const client = new Client({
+    url: GRAPH_API_URL,
+    exchanges: [cacheExchange, fetchExchange]
+})
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [tokens, setTokens] = useState<ITokens[]>([]);
+
+    async function fetchData() {
+        const response = await client.query(query, {}).toPromise();
+        setTokens(response.data.tokens);
+        console.log('🥶🥶🥶', response.data.tokens);
+
+    }
+
+    useEffect(() => {
+        fetchData().then()
+    }, [])
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div>
+            {tokens.map((token, index) => (
+                <div key={index}>
+                    <a href={token.contentURI} target={"_blank"}>Content URI</a>
+                    <p>{token.tokenID}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
